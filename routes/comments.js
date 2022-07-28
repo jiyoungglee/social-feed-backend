1e2dd1172e863140a75a3b534e79a6392306a052
Jiyoung Lee <jiyoung.g.lee118@gmail.com>
2022-07-28 12:51:49 -0400

    Get and update individual comments. Get filtered results for posts.

--- routes/comments.js
+++ routes/comments.js
@@ -4,9 +4,41 @@ var router = express.Router();
 
 router.get('/', (req,res) => {
   db.query(
-    `SELECT comments.*, users.username FROM comments
+    `SELECT comments.*, users.username as commenter FROM comments
     LEFT JOIN users ON comments.userId = users.userId
     ORDER BY commentTimestamp DESC`,
+    (err, result) => {
+      if(err) {
+        console.log(err);
+      }
+      res.send(result);
+    }
+  );
+});
+
+router.get('/getComments/:postId', (req,res) => {
+  db.query(
+    `SELECT comments.*, users.username as commenter FROM comments
+    LEFT JOIN users ON comments.userId = users.userId
+    WHERE postId=?
+    ORDER BY commentTimestamp DESC`,
+    [req.params.postId],
+    (err, result) => {
+      if(err) {
+        console.log(err);
+      }
+      res.send(result);
+    }
+  );
+});
+
+router.get('/getComment/:commentId', (req,res) => {
+  db.query(
+    `SELECT comments.*, users.username as commenter FROM comments
+    LEFT JOIN users ON comments.userId = users.userId
+    WHERE commentId=?
+    ORDER BY commentTimestamp DESC`,
+    [req.params.commentId],
     (err, result) => {
       if(err) {
         console.log(err);
@@ -19,7 +51,7 @@ router.get('/', (req,res) => {
 router.post('/insert', (req,res) => {
   try {
   db.query(
-    'INSERT INTO comments (userId, postId, commentText) VALUES (?, ?, ?)',
+    `INSERT INTO comments (userId, postId, commentText) VALUES (?, ?, ?)`,
     [req.body.userId, req.body.postId, req.body.commentText],
     (err, result) => {
       if(err) {
@@ -33,5 +65,60 @@ router.post('/insert', (req,res) => {
     res.status(500).send(error);
   }
 });
+
+//UDPATE LIKES
+router.put('/updatelikes', (req,res) => {
+  try {
+    db.query(
+      'UPDATE comments SET commentLikes=? WHERE commentId=?',
+      [req.body.commentLikes, req.body.commentId],
+      (err, result) => {
+        if(err) {
+          console.log(err);
+        }
+        res.send(result);
+      }
+    );
+    } catch(error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+})
+
+router.delete('/deletecomment', (req,res) => {
+  try {
+    db.query(
+      'DELETE FROM comments WHERE commentId=?',
+      [req.body.commentId],
+      (err, result) => {
+        if(err) {
+          console.log(err);
+        }
+        res.send(result);
+      }
+    );
+    } catch(error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+})
+
+router.put('/updatecomment', (req,res) => {
+  try {
+    db.query(
+      'UPDATE comments SET commentText=? WHERE commentId=?',
+      [req.body.commentText, req.body.commentId],
+      (err, result) => {
+        if(err) {
+          console.log(err);
+        }
+        res.send(result);
+      }
+    );
+    } catch(error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+})
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -2,35 +2,93 @@ var express = require('express');
 var db = require('../connection');
 var router = express.Router();
 
+// Get all posts
 router.get('/', (req,res) => {
   db.query(
-    `SELECT usersPost.username as poster, posts.*, JSON_ARRAYAGG(
-      JSON_OBJECT(
-        'commentId', comments.commentId, 
-        'commenter', usersComment.username,
-        'commentLikes', comments.commentLikes, 
-        'commentText', comments.commentText, 
-        'commentTimestamp', comments.commentTimestamp
-      )
-    ) as comments
+    `SELECT users.username as poster, posts.*, distinctFavorites.*, ifnull(numberComments, 0) commentsCount
     FROM posts
-    LEFT JOIN comments ON comments.postId = posts.id
-    LEFT JOIN users usersPost
-    ON usersPost.userId = posts.userId
-    LEFT JOIN users usersComment
-    ON usersComment.userId = comments.userId
-    GROUP BY posts.id
-    ORDER BY timestamp DESC`,
+    LEFT JOIN users ON users.userId = posts.userId
+    LEFT JOIN
+        (SELECT comments.*, users.username as commenter
+        FROM comments
+        LEFT JOIN users
+        ON users.userId = comments.userId
+        WHERE commentId IN (
+          SELECT MAX(commentId) AS commentId
+          FROM (
+              SELECT  a.id as postId, b.commentId, b.commentLikes
+              FROM posts a 
+              INNER JOIN comments b 
+              ON a.id = b.postId
+              INNER JOIN  (   
+                    SELECT      postId, MAX(commentLikes) maxLikes   
+                    FROM        comments   
+                    GROUP BY    postId  ) post_favoriteComments
+              ON  post_favoriteComments.postId = b.postId AND post_favoriteComments.maxLikes = b.commentLikes
+          ) allFavoriteComments
+        GROUP BY postId)) distinctFavorites
+    ON posts.id = distinctFavorites.postId
+    LEFT JOIN
+        (SELECT postId, COUNT(commentId) numberComments
+        FROM comments
+        GROUP BY postId) countRef
+    ON countRef.postId = posts.id
+    ORDER BY posts.timestamp DESC`,
     (err, result) => {
       if(err) {
         console.log(err);
       }
-      result.forEach(post => post.comments = JSON.parse(post.comments))
       res.send(result);
     }
   );
 });
 
+//Get filtered posts
+router.put('/getResults', (req,res) => {
+  db.query(
+    `SELECT filteredPosts.*, distinctFavorites.*, ifnull(numberComments, 0) commentsCount
+    FROM (
+		SELECT users.username as poster, posts.*
+    FROM posts
+		LEFT JOIN users ON users.userId = posts.userId
+		WHERE posts.postDetails LIKE ? OR users.username LIKE ?) filteredPosts
+    LEFT JOIN
+        (SELECT comments.*, users.username as commenter
+        FROM comments
+        LEFT JOIN users
+        ON users.userId = comments.userId
+        WHERE commentId IN (
+          SELECT MAX(commentId) AS commentId
+          FROM (
+              SELECT  a.id as postId, b.commentId, b.commentLikes
+              FROM posts a 
+              INNER JOIN comments b 
+              ON a.id = b.postId
+              INNER JOIN  (   
+                    SELECT      postId, MAX(commentLikes) maxLikes   
+                    FROM        comments   
+                    GROUP BY    postId  ) post_favoriteComments
+              ON  post_favoriteComments.postId = b.postId AND post_favoriteComments.maxLikes = b.commentLikes
+          ) allFavoriteComments
+        GROUP BY postId)) distinctFavorites
+    ON filteredPosts.id = distinctFavorites.postId
+    LEFT JOIN
+        (SELECT postId, COUNT(commentId) numberComments
+        FROM comments
+        GROUP BY postId) countRef
+    ON countRef.postId = filteredPosts.id
+    ORDER BY filteredPosts.timestamp DESC`,
+    [req.body.searchQuery, req.body.searchQuery],
+    (err, result) => {
+      if(err) {
+        console.log(err);
+      }
+      res.send(result);
+    }
+  );
+});
+
+// Add new post
 router.post('/insert', (req,res) => {
   try {
   db.query(
