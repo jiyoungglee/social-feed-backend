e08f89a73309035502ab8dfe66b987f351ebdb54
Jiyoung Lee <jiyoung.g.lee118@gmail.com>
2022-09-21 12:30:32 -0400

    Add SSL certificate

--- connection.js
+++ connection.js
@@ -1,11 +1,23 @@
+var fs = require('fs');
 const mysql = require('mysql');
 require('dotenv').config();
+
+const serverCa = [fs.readFileSync(process.env.CERTIFICATE, "utf8")];
 
 const db = mysql.createConnection({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  port:3306,
+  ssl: {
+      rejectUnauthorized: true,
+      ca: serverCa
+  }
+});
+
+db.connect(function(err) {
+if (err) throw err;
 });
 
 module.exports = db;
