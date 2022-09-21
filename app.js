54d7fbd4e2c85d2a8a0aceda06e50794483778c4
Jiyoung Lee <jiyoung.g.lee118@gmail.com>
2022-09-21 15:20:07 -0400

    handle options method

--- app.js
+++ app.js
@@ -25,6 +25,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  if ('OPTIONS' == req.method) {
+      return res.sendStatus(200);
+  } else {
+      next();
+  }
+});
+
 // Passport session
 app.use(session({
   key: 'userId',
